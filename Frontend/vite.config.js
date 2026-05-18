e4671507967f0782251de0552055d5cb07c08eea
Abhishek Bhatia <abhibhatia0410@gmail.com>
2026-05-19 01:36:19 +0530

    frontend fixed

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -11,5 +10,7 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,
+    allowedHosts: true,
+    hmr: false
   }
 })
