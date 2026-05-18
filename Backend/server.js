a157f1b131e37f4aaa928d567b3025724805af42
Abhishek Bhatia <abhibhatia0410@gmail.com>
2026-05-18 23:32:46 +0530

    Containerize done

--- Backend/server.js
+++ Backend/server.js
@@ -5,15 +5,16 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connect to database
 connectDB();
 
-// Routes
 app.use('/', require('./routes/urlRoutes'));
 
-const PORT = process.env.PORT || 5000;
+app.get('/health', (req, res) => {
+    res.send('Healthy');
+})
+console.log(process.env.MONGO_URI)
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
